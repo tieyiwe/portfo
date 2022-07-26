e9b8b8449a41432b964ac6a7908edce8085b2dd2
tieyiwe <60913036+tieyiwe@users.noreply.github.com>
2022-07-26 16:22:02 -0400

    Update server.py
    corrected a mistake and move a snippet around

--- server.py
+++ server.py
@@ -9,6 +9,11 @@ app = Flask(__name__)
 def my_home():
     return render_template('index.html')
 
+#this allows pages to be added dynamically to the server we do not need to manually ad them
+@app.route('/<string:page_name>')
+def html_page(page_name):                       
+    return render_template(page_name)
+
 def write_to_file(data):
     with open('database.txt', mode='a') as database:
               email = data['email']
@@ -25,10 +30,7 @@ def write_to_csv(data):
               csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
               csv_writer.writerow([email,subject, message,logger])
 
-#this allows pages to be added dynamically to the server we do not need to manually ad them
-@app.route('/<string:page_name>')
-def html_page(page_name):                       
-    return render_template(page_name)
+
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
